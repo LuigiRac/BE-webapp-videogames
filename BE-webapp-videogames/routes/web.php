65e1f38e02e085b493f908f5b5c7f615e0253dbe
LuigiRac <lui.rac06@gmail.com>
2025-05-26 11:24:27 +0200

    seed db

--- BE-webapp-videogames/routes/web.php
+++ BE-webapp-videogames/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\DashboardController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,5 +18,15 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::middleware(['auth', 'verified'])
+->name('admin.')
+->prefix('admin')
+->group(
+
+    function(){
+        Route::get('/index', [DashboardController::class , 'index']) ->name('index');
+    }
+);
 
 require __DIR__.'/auth.php';
