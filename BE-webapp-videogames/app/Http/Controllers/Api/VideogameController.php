22cc1cd57a1a0224431bc582b93455b776d9a188
LuigiRac <lui.rac06@gmail.com>
2025-05-29 11:57:35 +0200

    Json response

--- BE-webapp-videogames/app/Http/Controllers/Api/VideogameController.php
+++ BE-webapp-videogames/app/Http/Controllers/Api/VideogameController.php
@@ -5,9 +5,20 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Videogame;
+
 class VideogameController extends Controller
 {
     public function index(){
-        return 'Sei nella index API';
+
+        $videogames = Videogame::all();
+        // dd($videogames);
+
+        return response()->json(
+            [
+                'success'=>true,
+                'data'=>'tutto funziona bene'
+            ]
+        );
     }
 }
